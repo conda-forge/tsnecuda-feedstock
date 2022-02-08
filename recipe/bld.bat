89728ef1ce6f4a0378b682a62f19f98ce75f6f5b
H. Vetinari <h.vetinari@gmx.com>
2022-02-08 19:37:38 +1100

    add some win-cuda workarounds

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 SetLocal EnableDelayedExpansion
 
-REM %MY_VAR:~0,2% selects first two characters
+:: %MY_VAR:~0,2% selects first two characters
 if "%cuda_compiler_version:~0,2%"=="10" (
     set "CMAKE_CUDA_ARCHS=35-real;50-real;52-real;60-real;61-real;70-real;75"
 )
@@ -18,6 +18,14 @@ if "%cuda_compiler_version:~0,2%"=="11" (
 set CUDA_CONFIG_ARGS=-DCMAKE_CUDA_ARCHITECTURES=!CMAKE_CUDA_ARCHS!
 :: cmake does not generate output for the call below; echo some info
 echo Set up extra cmake-args: CUDA_CONFIG_ARGS=!CUDA_CONFIG_ARGS!
+
+:: See more extensive comment in build-lib.sh
+:: TODO: Fix this in nvcc-feedstock or cmake-feedstock.
+del %BUILD_PREFIX%\bin\nvcc.bat
+
+:: ... and another workaround just to cover more bases
+set "CudaToolkitDir=%CUDA_PATH%"
+set "CUDAToolkit_ROOT=%CUDA_PATH%"
 
 mkdir build
 cd build
